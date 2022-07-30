a370218987ef4705ab9490efacf162ddb540e5cc
robotcas <97564481+robotcas@users.noreply.github.com>
2022-07-29 23:51:06 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+  grabCursor:true,
+});
+
+var swiper = new Swiper(".home-slider", {
+   grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+});
+
+'use strict';
+const toggleBtn = document.querySelectorAll('.faq__item--btns');
+
+toggleBtn.forEach(btns => {
+  btns.addEventListener('click', () => {
+    btns.parentNode.classList.toggle('active');
+  });
+});
